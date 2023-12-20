1112cc9d83ec5f467759b76fd423de1cc5fb0c5d
GloryDream413 <glorydream413@gmail.com>
2023-12-21 04:38:25 +0900

    TestToken deploying script updated.

--- DeployProject/scripts/networks/core.js
+++ DeployProject/scripts/networks/core.js
@@ -1,15 +1,19 @@
-const deployWBTC = require("../deployWBTC.js");
+const deployKLAY = require("../deployKLAY.js");
+const deployLINK = require("../deployLINK.js");
+const deployTRX = require("../deployTRX.js");
+const deployWBNB = require("../deployWBNB.js");
 const deployWETH = require("../deployWETH.js");
-const deployEUR = require("../deployEUR.js");
-const deployXAU = require("../deployXAU.js");
-const deployUSDC = require("../deployUSDC.js");
+const deployWMATIC = require("../deployWMATIC.js");
+const deployUSDT = require("../deployUSDT.js");
 
 const deploy_core = async () => {
-    await deployEUR();
-    // await deployXAU();
-    // await deployWETH();
-    // await deployWBTC();
-    // await deployUSDC();
+    await deployKLAY();
+    await deployLINK();
+    await deployTRX();
+    await deployWBNB();
+    await deployWETH();
+    await deployWMATIC();
+    await deployUSDT();
 }
 
 module.exports = { deploy_core };
