cac8e321a8f3e9add176c284e197df22b86b8c05
GloryDream413 <glorydream413@gmail.com>
2023-12-21 04:09:54 +0900

    Test Token deploying script update.

--- DeployProject/migrations/2_contract_deployment.js
+++ DeployProject/migrations/2_contract_deployment.js
@@ -1,18 +1,30 @@
-const WAVAX = artifacts.require("WAVAX");
-const WBTC = artifacts.require("WBTC");
+const WMATIC = artifacts.require("WMATIC");
+const WBNB = artifacts.require("WBNB");
 const WETH = artifacts.require("WETH");
-const USDC = artifacts.require("USDC");
+const LINK = artifacts.require("LINK");
+const TRX = artifacts.require("TRX");
+const KLAY = artifacts.require("KLAY");
+const USDT = artifacts.require("USDT");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(WAVAX);
-  const WAVAXDeployed = await WAVAX.deployed();
+  await deployer.deploy(WMATIC);
+  const WMATICDeployed = await WMATIC.deployed();
 
-  await deployer.deploy(WBTC);
-  const WBTCDeployed = await WBTC.deployed();
+  await deployer.deploy(WBNB);
+  const WBNBDeployed = await WBNB.deployed();
 
   await deployer.deploy(WETH);
   const WETHDeployed = await WETH.deployed();
 
-  await deployer.deploy(USDC);
-  const USDCDeployed = await USDC.deployed();
+  await deployer.deploy(LINK);
+  const LINKDeployed = await LINK.deployed();
+
+  await deployer.deploy(TRX);
+  const TRXDeployed = await TRX.deployed();
+
+  await deployer.deploy(KLAY);
+  const KLAYDeployed = await KLAY.deployed();
+
+  await deployer.deploy(USDT);
+  const USDTDeployed = await USDT.deployed();
 };
